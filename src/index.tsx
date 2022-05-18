ad72a7852bd4e6ee94a7b4426d2fbf042596f3a7
dzennank <dzenankrlic@gmail.com>
2022-05-18 13:20:48 +0200

    .

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,24 @@
 import React from "react";
 import ReactDOM  from "react-dom";
 
-class App extends React.Component {
-    render() {
-        return <h1>Hi there!</h1>
-    }
+// class App extends React.Component {
+//     render() {
+//         return <h1>Hi there!</h1>
+//     }
+// }
+
+
+
+interface AppProps {
+    color?: string
 }
 
+const App = (props: AppProps): JSX.Element => {
+    return <div>{props.color}</div>
+}
+
+
 ReactDOM.render(
-    <App />,
+    <App color="red"/>,
     document.getElementById('root')
 )
